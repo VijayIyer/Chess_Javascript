e2d5daa245c23c3a92f9041eed47e88a433ee72e
vijay <vsiyer@github.iu.edu>
2022-05-15 12:25:05 -0400

    timer count logic

--- movingPieces.js
+++ movingPieces.js
@@ -1,20 +1,51 @@
-timer1 = document.getElementById("player1_timer");
-timer2 = document.getElementById("player2_timer");
-timer1.minutes = 5;
-timer1.seconds = 60;
-timer2.minutes = 5;
-timer2.seconds = 60;
+
+
+
+function onload()
+{
+
+	timer1 = document.getElementById("player1_timer");
+	timer2 = document.getElementById("player2_timer");
+	timer1.clicked = false;
+	timer2.clicked = false;
+	timer1.minutes = 5;
+	timer1.seconds = 60;
+	timer2.minutes = 5;
+	timer2.seconds = 60;
+	setPlayerTimers();
+}
 
 function setPlayerTimers()
 {
-	document.getElementById("player1_timer").innerHTML = "5:00";
-	document.getElementById("player2_timer").innerHTML = "5:00";
+	
+	document.getElementById("player1_timer").innerHTML =	timer1.minutes+":"+(timer1.seconds%60).toLocaleString("en-US", {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+});
+	document.getElementById("player2_timer").innerHTML =  timer2.minutes+":"+(timer2.seconds%60).toLocaleString("en-US", {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+});
+
 }
 
 function startCountDown()
 {
+	
 	timer_id = event.target.id;
 	
 	timer = document.getElementById(timer_id);
+	if(timer.clicked == false)
+	{	timer.clicked = true;
+		timer.intervalID = setInterval(() => {
+			if(timer.seconds%60 == 0) timer.minutes--;
+			timer.seconds--;
+			setPlayerTimers();
+			}, 1000);
 	
+	}
+	else if(timer.clicked == true) {
+	clearInterval(timer.intervalID); 
+	timer.clicked = false;
+	}
 }
